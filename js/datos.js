0cc5adabdded46a552e70bc8120263ad97bb1194
ricardobe1 <102460673+ricardobe1@users.noreply.github.com>
2022-03-31 01:45:19 -0600

    Add files via upload

---
+++ js/datos.js
@@ -0,0 +1,18 @@
+var string="BIENVENIDO"; //Tipo string
+var int=316; //Tipo int
+var float=4.7; //Tipo Float
+var boolean=false; //Tipo boleano
+var undefined; //Undefined
+var mensaje = "Bienvenido a nuestro sitio web";
+var nombreProducto = "Producto ABC";
+var letraSeleccionada = "c";
+
+document.write("   '"+string + "' Variable de Tipo String //");
+document.write("<br>");
+document.write("   '"+int + "' Variable de Tipo Int //");
+document.write("<br>");
+document.write("   '"+Symbol + "' Variable de Tipo Simbolica //");
+document.write("<br>");
+document.write("   '"+Number + "' Variable de Tipo numeritca //");
+document.write("<br>");
+document.write("   '"+undefined + "' Variable de Tipo Indefinida //");
